c891a506809528e5e2c4cbc99f74ce80ff6dd22b
DuckenClaw <ducken.claw@gmail.com>
2025-02-19 17:26:06 +0100

    changed netlify deploy

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -140,3 +140,13 @@ bot.onText(/\/menu/, (msg) => {
     }
   });
 });
+
+exports.handler = async event => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body))
+    return { statusCode: 200, body: "" }
+  } catch (e) {
+    console.error("error in handler:", e)
+    return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+  }
+}
